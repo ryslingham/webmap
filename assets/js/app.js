7cd6852eee0b2c3fe201d9c8d8db488eefb1b1a5
ryslingham <rjstuart87@gmail.com>
2016-11-19 14:19:18 +1000

    added js, css and index.html

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicnlzbGluZ2hhbSIsImEiOiJjaXZua2hyb3kwMGFoMnRxdnUyenNucWR3In0.U_dIpzTGoLCZgKsSFiSGkA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v9',
+    zoom: '14',
+    center: [139.497467, -20.724705],
+    minZoom: '14',
+    maxZoom: '14'
+});
